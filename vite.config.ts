ea9a9ae5cd7bb5aaa0b2356885db910392c19c24
Rusiru Promod <rusirupromod@gmail.com>
2025-11-16 00:51:42 +0530

    next

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/gold-peak-aesthetics/" // must match your repo name
-});
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/gold-peak-aesthetics/" : "/", // 🔥 important for GitHub Pages
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
